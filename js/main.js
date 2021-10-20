7dff93338c6779bf56d6111e4b78aef5476986bd
aleksandrbelokon <aleksandrbelokon@yandex.ru>
2021-10-20 20:22:10 +0300

    Проверка корректности заполнения формы средствами JavaScript

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
-var text = document.getElementById('text'); //Выбираем объект с которым будем работать, выбираем по id 
-text.style.color = "red"; //Устанавливаем цвет текста
-text.style.backgroundColor = "blue"; //Устанавливаем цвет фона
-
-text.innerHTML = "Какойто текст"; //Записываем текст в HTML тег
-
-document.getElementById('text').style.backgroundColor = "white";
-
-//Обрабатываем массив элементов span
-//var spans = document.getElementsByTagName('span');
-
-//Обрабатываем массив элементов span "теперь обращаемся по имени класса"
-var spans = document.getElementsByClassName('simple-text');
-for (var i=0; i<spans.length; i++) {
-    spans[i].innerHTML =   "- " + i + "<br>";
+function checkForm(el) 
+{
+    
+    var name = el.name.value;     // Получает имя
+    var pass = el.pass.value;     // Получаем пароль
+    var repass = el.repass.value; // Получаем подтрержденный пароль
+    var state = el.state.value;   // Получаем пол
+    
+    var fail = "";
+    
+    if (name == "" || pass == "" || state == "") 
+        fail = "Заполните все поля";
+    else if (name.length <= 1 || name.length > 50) 
+        fail = "Введите корректное имя";
+    else if (pass != repass) 
+        fail = "Пароли должны совпадать";
+    else if (pass.split("&").length > 1) 
+        fail = "Недопустимый символ &";
+    
+    if (fail != "") {
+        document.getElementById('error').innerHTML = fail + "<br><br>";
+        return false;
+    } else {
+        alert("Все данные корректно заполненны")
+        return true;    
+    }   
 }
